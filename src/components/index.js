8bde8cb0927b9ebe8d93299d5160230548a85b05
Tanmay Pal <tanmayspal2003@gmail.com>
2024-06-17 13:04:44 +0530

    Added index page in components for easy import

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import SearchHistoryPopup from "./Header/SearchHistoryPopup";
+import UserProfilePopup from "./Header/UserProfilePopup";
+import SideBar from "./SideBar/SideBar";
+import AuthLayout from "./AuthLayout";
+import LandingPage from "./LandingPage";
+import LoginForm from "./LoginForm";
+import Player from "./Player";
+import VideoCard from "./VideoCard";
+import VideoList from "./VideoList";
+import VideoUploadForm from "./VideoUploadForm";
+
+export {
+    Header,
+    LogoutBtn,
+    SearchHistoryPopup,
+    UserProfilePopup,
+    SideBar,
+    AuthLayout,
+    LandingPage,
+    LoginForm,
+    Player,
+    VideoCard,
+    VideoList,
+    VideoUploadForm,
+}
