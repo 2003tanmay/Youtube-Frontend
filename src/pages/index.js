1caa64cdc9a97c182072df4ca9b4e64e109c7f41
Tanmay Pal <tanmayspal2003@gmail.com>
2024-06-17 13:06:12 +0530

    Added index page in pages for easy import

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import Home from "./Home";
+import History from "./History";
+import LikedVideos from "./LikedVideos";
+import Login from "./Login";
+import SearchPage from "./SearchPage";
+import Subscription from "./Subscription";
+import You from "./You";
+import YourChannel from "./YourChannel";
+import Tweets from "./Tweets";
+import UploadVideo from "./UploadVideo";
+import WatchVideo from "./WatchVideo";
+
+export {
+    Home,
+    History,
+    LikedVideos,
+    Login,
+    SearchPage,
+    Subscription,
+    You,
+    YourChannel,
+    Tweets,
+    UploadVideo,
+    WatchVideo
+}
