848b652df2d1598233f9ecf5d286e2f440d831a6
Tanmay Pal <tanmayspal2003@gmail.com>
2024-06-17 13:11:13 +0530

    Added sidebar reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,13 @@
 import { combineReducers } from "redux";
 
 // reducers
-import userSlice from "./user.reducer.js";
 import channelSlice from "./channel.reducer.js";
 import playbackSlice from "./playback.reducer.js";
 import playlistSlice from "./playlist.reducer.js";
 import searchSlice from "./search.reducer.js";
+import sidebarSlice from "./sidebar.reducer.js";
 import tweetSlice from "./tweet.reducer.js";
+import userSlice from "./user.reducer.js";
 import videoSlice from "./video.reducer.js";
 
 export default combineReducers({
@@ -16,5 +17,6 @@ export default combineReducers({
   playlist: playlistSlice,
   search: searchSlice,
   tweet: tweetSlice,
-  video: videoSlice
+  video: videoSlice,
+  sidebar: sidebarSlice
 });
